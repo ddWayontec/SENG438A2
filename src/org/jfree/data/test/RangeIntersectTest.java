11eb3b08236f86a8e702b06fbf0dae50d2454af7
ddWayontec <demisangdan@gmail.com>
2020-02-05 14:26:11 -0700

    added intersects tests

---
+++ src/org/jfree/data/test/RangeIntersectTest.java
@@ -0,0 +1,43 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RangeIntersectTest {
+	public static Range controlRange;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		controlRange = new Range (1, 10);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void rangePartiallyIntersect() {
+		Boolean intersectsResult = controlRange.intersects(8, 15);
+		assertFalse(intersectsResult);
+	}
+	
+	@Test
+	public void rangeDoesNotIntersect() {
+		Boolean intersectsResult = controlRange.intersects(11, 15);
+		assertFalse(intersectsResult);
+	}
+	
+	@Test
+	public void rangeIntersects() {
+		Boolean intersectsResult = controlRange.intersects(5, 8);
+		assertTrue(intersectsResult);
+	}
+	
+	
+
+
+}
