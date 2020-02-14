fa3876962dd66ad6fbf145c7149a8d7ec1435c8c
ddWayontec <demisangdan@gmail.com>
2020-02-14 01:22:03 -0700

    finished intersect test and started columntotal test

--- src/org/jfree/data/test/DataUtilitiesCalculateColumnTotalTest.java
+++ src/org/jfree/data/test/DataUtilitiesCalculateColumnTotalTest.java
@@ -2,6 +2,10 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.*;
 
+import org.jfree.data.DataUtilities;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,21 +18,27 @@ public class DataUtilitiesCalculateColumnTotalTest {
 	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void validPositiveColumn() {
+		Mockery mockingContext = new Mockery(); 
+		final Values2D values = mockingContext.mock(Values2D.class);
+		
+		mockingContext.checking(new Expectations() { 
+			{
+				one(values).getRowCount(); 
+				will(returnValue(2));
+				
+				one(values).getValue(0, 0);
+				will(returnValue(3));
+				
+				one(values).getValue(0, 1); 
+				will(returnValue(5));
+				
+			} 
+		});
+		
+		//double result = DataUtilities.calculateColumnTotal(data, column);
+		
 	}
 
 }
--- src/org/jfree/data/test/RangeIntersectTest.java
+++ src/org/jfree/data/test/RangeIntersectTest.java
@@ -7,32 +7,77 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RangeIntersectTest {
-	public static Range controlRange;
+	public static Range positiveControlRange;
+	public static Range negativeControlRange;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		controlRange = new Range (1, 10);
+		positiveControlRange = new Range (2, 10);
+		negativeControlRange = new Range (-16, -8);
 	}
 
 	@Test
-	public void rangePartiallyIntersect() {
-		Boolean intersectsResult = controlRange.intersects(8, 15);
-		assertFalse(intersectsResult);
+	public void positiveRangeIntersectsAtEnd() {
+		Boolean intersectsResult = positiveControlRange.intersects(8, 15);
+		assertTrue("positive intersection of range at tail end returns true", intersectsResult);
 	}
 	
 	@Test
-	public void rangeDoesNotIntersect() {
-		Boolean intersectsResult = controlRange.intersects(11, 15);
-		assertFalse(intersectsResult);
+	public void positiveRangeIntersectsAtStart() {
+		Boolean intersectsResult = positiveControlRange.intersects(0, 4);
+		assertTrue("positive intersection of range at start returns true", intersectsResult);
 	}
 	
 	@Test
-	public void rangeIntersects() {
-		Boolean intersectsResult = controlRange.intersects(5, 8);
-		assertTrue(intersectsResult);
+	public void greaterPositiveRangeDoesNotIntersect() {
+		Boolean intersectsResult = positiveControlRange.intersects(11, 15);
+		assertFalse("positive range greater than compared range does not intersect", intersectsResult);
+	}
+	
+	@Test
+	public void smallerPositiveRangeDoesNotIntersect() {
+		Boolean intersectsResult = positiveControlRange.intersects(0, 1);
+		assertFalse("positive range smaller than compared range does not intersect", intersectsResult);
+	}
+	
+	@Test
+	public void positiveRangeCompleteOverlapIntersects() {
+		Boolean intersectsResult = positiveControlRange.intersects(5, 8);
+		assertTrue("positive range that is subset of another range intersects", intersectsResult);
+	}
+	
+	//same tests but with negative numbers
+	
+	@Test
+	public void negativeRangeIntersectsAtEnd() {
+		Boolean intersectsResult = negativeControlRange.intersects(-10, -5);
+		assertTrue("negative intersection of range at tail end returns true", intersectsResult);
+	}
+	
+	@Test
+	public void negativeRangeIntersectsAtStart() {
+		Boolean intersectsResult = negativeControlRange.intersects(-20, -14);
+		assertTrue("negative intersection of range at start returns true", intersectsResult);
+	}
+	
+	@Test
+	public void greaterNegativeRangeDoesNotIntersect() {
+		Boolean intersectsResult = negativeControlRange.intersects(-7, -3);
+		assertFalse("negative range greater than compared range does not intersect", intersectsResult);
+	}
+	
+	@Test
+	public void smallerNegativeRangeDoesNotIntersect() {
+		Boolean intersectsResult = negativeControlRange.intersects(-30, -20);
+		assertFalse("negative range smaller than compared range does not intersect", intersectsResult);
 	}
 	
 	
+	@Test
+	public void negativeRangeCompleteOverlapIntersects() {
+		Boolean intersectsResult = negativeControlRange.intersects(-13, -10);
+		assertTrue("range that is subset of another range intersects", intersectsResult);
+	}
 
 
 }
