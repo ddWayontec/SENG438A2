b39f4a404b178a9d083b2cecd5ec67cd884fef87
unknown <rish.kumaria@qhrtech.com>
2020-02-13 18:40:28 -0700

    Added more tests

--- src/org/jfree/data/test/RangeEqualsTest.java
+++ src/org/jfree/data/test/RangeEqualsTest.java
@@ -31,5 +31,23 @@ public class RangeEqualsTest {
 		Range notEqualObject = new Range(0, 1);
 		assertFalse( rangeObject.equals(notEqualObject) );
 	}
+	
+	@Test
+	public void combinedRangeEqualsTest() {
+		Range object1 = new Range(0,1);
+		Range object2 = new Range(1,4);
+		Range object3 = Range.combine(object2, object1);
+		Range object4 = new Range(1,4);
+		assertTrue(object4.equals(object3) );
+	}
+	
+	@Test 
+	public void combinedRangeNotEqualsTest() {
+		Range object1 = new Range(0,1);
+		Range object2 = new Range(1,4);
+		Range object3 = Range.combine(object2, object1);
+		Range object4 = new Range(0,4);
+		assertFalse(object4.equals(object3) );
+	}
 
 }
